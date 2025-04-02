8bbdc1de84b6bdc705f5540b3e843abbc181f2c8
ElvBry <elvar.bryhni@gmail.com>
2025-04-02 17:21:49 +0200

    Update ESP_DSP_new.c
    Fungerende versjon med simulert data fra FPGA. Trenger forbedring for å unngå høyt antall falsk positive målinger

--- ESP-IDF/ESP_DSP_new.c
+++ ESP-IDF/ESP_DSP_new.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,20 +21,21 @@
 #define PIN_NUM_MOSI 23 // not used but full duplex is more synchronised
 #define PIN_NUM_CLK  18
 
-#define BUFFER_SIZE 20000
+#define BUFFER_SIZE 3600
 #define REDUCED_BUFFER_SIZE BUFFER_SIZE/4
 
 #define PI 3.14159265358979323846
 #define SAMPLE_FREQUENCY 390000
 
 #define TARGET_FREQUENCY_1 39000
-#define SIGNAL_THRESHOLD_39kHz 200
-#define NORMALIZED_THRESHOLD_39kHz 600000
-#define BINARY_SEARCH_STEPS_39kHz 15
+#define SIGNAL_THRESHOLD_39kHz 200000
+#define NORMALIZED_THRESHOLD_39kHz 400000
+#define BINARY_SEARCH_STEPS_39kHz 20
 #define REF_TABLE_SIZE_39kHz 10
 
-#define MAX_SIGNAL_DIFFERENCE_US 1000000
-#define MAX_SIGNAL_DURATION_US 1500
+#define MAX_SIGNAL_DIFFERENCE_US 800
+#define MAX_SIGNAL_DURATION_US 30000
+#define SIGNAL_Q15_MAX 8192
 
 #define MAX_CYCLE_COUNT 2*240*REDUCED_BUFFER_SIZE // estimate of CPU_FREQUENCY*REDUCED_BUFFER_SIZE/SAMPLE_FREQYENCY
 
@@ -46,6 +48,7 @@ static const char *TAG = "TDOA_DSP";
 typedef struct {
     uint32_t frequency;
     uint64_t last_ping_time_us;
+    uint64_t cooldown_until_us;
     int16_t *ref_sine;
     int16_t *ref_cos;
     uint16_t ref_table_len;
@@ -57,7 +60,18 @@ typedef struct {
     uint64_t arrival_times[NUM_RECEIVERS]; // Arrival timestamps for each receiver
 } frequency_state_t;
 
+void static inline print_buffer_csv_inline(const int16_t *buffer, const size_t len) {
+    char line[2048] = {0};
+    char temp[16];
 
+    for (size_t i = 0; i < len; i++) {
+        snprintf(temp, sizeof(temp), "%d", buffer[i]);
+        strcat(line, temp);
+        if (i < len - 1) strcat(line, ",");
+    }
+
+    ESP_LOGI(TAG, "%s", line);
+}
 
 //
 //  SIGNAL PROCESSING
@@ -70,7 +84,7 @@ int16_t ref_cos_table_Q15_39kHz[REF_TABLE_SIZE_39kHz] __attribute__((aligned(16)
 void initialize_sine_table_Q15(int16_t *src,const uint32_t target_frequency, const uint8_t len) {
     double angleStep = 2*PI*target_frequency/SAMPLE_FREQUENCY;
     for (int i = 0; i < len; i++) {
-        double value = sin(i*angleStep)/len; // divide by tablesize to avoid overflow in dotproduct calculation
+        double value = sin(i*angleStep)/len; // divide by a value larger than tablesize to avoid overflow in dotproduct calculation
         src[i] = (int16_t)(value * 32767.0); // converting to Q15 format. 32767 = 2^15 -1 
     }
 }
@@ -95,7 +109,7 @@ static inline void remove_dc_offset(int16_t* buffer, const uint16_t len) {
     int32_t sum = 0;
     for (int i = 0; i < 10; i++) sum += buffer[i];
 
-    int16_t offset = (int16_t)(sum / 10);
+    int16_t offset = (int16_t)(sum / len);
 
     for (int i = 0; i < len; i++) buffer[i] -= offset;
 }
@@ -121,30 +135,33 @@ static inline uint64_t calculate_signal_strength(const int16_t *ref_cos, const i
     return result;
 }
 
-// normalizes array to Q15 values before dividing by tablesize in order avoid problems in dotproduct function
+// normalizes array to Q15 values withing safemax in order avoid problems in dotproduct function
 static inline void normalize_signal_arr_Q15(int16_t *signal_arr, const uint16_t len, const uint8_t table_len) {
     int16_t abs_max = 0;
     for (size_t i = 0; i < len; i++) {
         int16_t abs_val = (signal_arr[i] < 0) ? -signal_arr[i] : signal_arr[i];
         if (abs_val > abs_max) abs_max = abs_val;
     }
-    if(abs_max == 0) abs_max = 1;
-    int32_t scale_factor = ((int32_t)32767 << 15) / abs_max;
+    if (abs_max == 0) {
+        ESP_LOGE(TAG, "Tried to normalize all 0 array");
+        abs_max = 1;
+    }
+
+    // Limit scale to Q15_SAFE_MAX to prevent dot product overflow
+    int32_t scale_factor = ((int32_t)SIGNAL_Q15_MAX << 15) / abs_max;
 
     for (int i = 0; i < len; i++) {
         int32_t temp = (int32_t)signal_arr[i] * scale_factor;
         signal_arr[i] = (int16_t)(temp >> 15);
-        signal_arr[i] /= table_len; // avoid overflow in dotproduct calculation
     }
 }
 
-
-static inline bool signalPresent(const int16_t *src, const frequency_state_t *freq_state) { 
+static inline bool signalPresent(const int16_t *src, const frequency_state_t *freq_state, const uint16_t len) { 
 
     uint64_t strength = calculate_signal_strength(
         freq_state->ref_cos, 
         freq_state->ref_sine, 
-        &src[REDUCED_BUFFER_SIZE - freq_state->ref_table_len], 
+        &src[len - freq_state->ref_table_len], 
         freq_state->ref_table_len
     );
     ESP_LOGD(TAG, "Calculated signal strength: %" PRIu64, strength);
@@ -166,7 +183,10 @@ static inline uint16_t binary_search_find_start_index(const int16_t *src, const 
     }
     uint64_t strength_high = calculate_signal_strength(freq_state->ref_cos, freq_state->ref_sine, &src[high], freq_state->ref_table_len);
     ESP_LOGD(TAG, "Calculated normalized strength high: %" PRIu64, strength_high);
-    if (strength_high < freq_state->normalized_threshold) return 0xFF; // Error: the signal in the upper window is below threshold
+    if (strength_high < freq_state->normalized_threshold) {
+        ESP_LOGE(TAG, "strength high: %lld", strength_high);
+        return 0xFF; // Error: the signal in the upper window is below threshold
+    }
 
     while (low <= high && steps < freq_state->binary_search_steps) {
         uint16_t mid = low + ((high - low) >> 1);
@@ -247,33 +267,34 @@ void reset_FPGA_output() {
 //  TASKS
 //
 
-void static inline print_buffer_csv_inline(const int16_t *buffer, const size_t len) {
-    char line[2048] = {0};
-    char temp[16];
 
-    for (size_t i = 0; i < len; i++) {
-        snprintf(temp, sizeof(temp), "%d", buffer[i]);
-        strcat(line, temp);
-        if (i < len - 1) strcat(line, ",");
-    }
 
-    ESP_LOGI(TAG, "%s", line);
-}
-
-// Prints receiver timestamps in format 
+// Prints timestamps in microseconds relative to the receiver that received ping first
 void print_receiver_values_csv(const frequency_state_t *freq_state) {
     char line[256] = {0};
     char temp[64];
-    
+
     snprintf(line, sizeof(line), "%" PRIu32, freq_state->frequency);
+
+    // Find the minimum timestamp
+    uint64_t min_time = freq_state->arrival_times[0];
+    for (int i = 1; i < NUM_RECEIVERS; i++) {
+        if (freq_state->arrival_times[i] < min_time) {
+            min_time = freq_state->arrival_times[i];
+        }
+    }
+
+    // Append each offset value (always positive)
     for (int i = 0; i < NUM_RECEIVERS; i++) {
-        snprintf(temp, sizeof(temp), "R%d: %" PRIu64, i + 1, freq_state->arrival_times[i]);
+        uint64_t delta = freq_state->arrival_times[i] - min_time;
+        snprintf(temp, sizeof(temp), "R%d: %" PRIu64, i + 1, delta);
         strcat(line, temp);
         if (i < NUM_RECEIVERS - 1) strcat(line, ", ");
     }
-    
+
     ESP_LOGI(TAG, "%s", line);
 }
+
 
 // Returns cycle count for debugging and testing
 static inline uint32_t get_ccount(void) {
@@ -284,9 +305,9 @@ static inline uint32_t get_ccount(void) {
 
 
 static inline bool error_check_cycle(const int64_t maxCycleCount, const uint64_t startCycle, const uint8_t index) {
-    int64_t currentTime = esp_timer_get_time();
-    if(startCycle + maxCycleCount < currentTime) {
-        ESP_LOGE(TAG,"ERROR: cycleCount too high %u, cycle: %lld", index, currentTime-startCycle);
+    int64_t currentCycle = get_ccount();
+    if(startCycle + maxCycleCount < currentCycle) {
+        ESP_LOGE(TAG,"ERROR: cycleCount too high %u, cycle: %lld", index, currentCycle-startCycle);
         return true;
     }
     return false;
@@ -299,9 +320,21 @@ static inline void update_frequency_state(int16_t *src, frequency_state_t *freq_
         ESP_LOGD(TAG, "Skip processing receiver: %u: ", index);
         return; // Avoid looking for already found signal
     }
+    // Second measurement of signal
+    if(freq_state->arrival_times[index] != 0) {
+        if(signalPresent(src, freq_state, freq_state->ref_table_len)) {
+            freq_state->valid_flags[index] = true;
+            for (int i = 0; i < NUM_RECEIVERS; i++) {
+                if(!freq_state->valid_flags[i]) break;
+                if(i == NUM_RECEIVERS -1) freq_state->all_valid = true;
+            }
+        } else freq_state->arrival_times[index] = 0;
+        return;
+    }
+   
     //if(src[1] != 0) print_buffer_csv_inline(src, REDUCED_BUFFER_SIZE);
     ESP_LOGD(TAG, "Processing signal");
-    if(!signalPresent(src, freq_state)) {
+    if(!signalPresent(src, freq_state, REDUCED_BUFFER_SIZE)) {
         ESP_LOGD(TAG, "Signal processing failed");
         return; // Initial check for signal in order to avoid normalizing noise which breaks binary search
     }
@@ -311,7 +344,11 @@ static inline void update_frequency_state(int16_t *src, frequency_state_t *freq_
         src[REDUCED_BUFFER_SIZE] = (int16_t)true;
     }
     uint16_t estimated_start_index = binary_search_find_start_index(src, freq_state);
-    if(estimated_start_index == 0xFF) return;
+    if(estimated_start_index == 0xFF) {
+        //print_buffer_csv_inline(src+REDUCED_BUFFER_SIZE/4, REDUCED_BUFFER_SIZE/4);
+        //ESP_LOGE(TAG, "could not find signal after normalization");
+        return;
+    }
     
     ESP_LOGD(TAG,"Found signal receiver: %u, cycles: %lld", index, get_ccount()-startCycle);
     //print_buffer_csv_inline(src+estimated_start_index, REDUCED_BUFFER_SIZE-estimated_start_index);
@@ -322,24 +359,14 @@ static inline void update_frequency_state(int16_t *src, frequency_state_t *freq_
     print_buffer_csv_inline(rec4_arr, REDUCED_BUFFER_SIZE);
     */
     uint64_t estimated_arrival_time_us = calculate_arrival_time_us(end_time_us,estimated_start_index, REDUCED_BUFFER_SIZE);
-    if(freq_state->arrival_times[index] == 0) { // First successful measurement of signal
-        if(estimated_arrival_time_us > freq_state->last_ping_time_us) freq_state->last_ping_time_us = estimated_arrival_time_us;
-        freq_state->arrival_times[index] = estimated_arrival_time_us;
-        //return;
-    }
-
-    
-    // Second successful measurement of signal
-    
-    freq_state->valid_flags[index] = true;
-    for (int i = 0; i < NUM_RECEIVERS; i++) {
-        if(!freq_state->valid_flags[i]) break;
-        if(i == NUM_RECEIVERS -1) freq_state->all_valid = true;
-    }
+    if(estimated_arrival_time_us > freq_state->last_ping_time_us)
+        freq_state->last_ping_time_us = estimated_arrival_time_us;
+    freq_state->arrival_times[index] = estimated_arrival_time_us; // First successful measurement of signal
 }
 
 // Allow frequency to be measured again by setting its updatable values to 0
 static inline void reset_frequency_state(frequency_state_t *freq_state) {
+    freq_state->cooldown_until_us = esp_timer_get_time() + MAX_SIGNAL_DURATION_US;
     freq_state->all_valid = false;
     freq_state->last_ping_time_us = 0;
     for(int i = 0; i < NUM_RECEIVERS; i++) {
@@ -349,6 +376,7 @@ static inline void reset_frequency_state(frequency_state_t *freq_state) {
 }
 
 frequency_state_t frequency_states[NUM_FREQUENCIES] = {{
+    .cooldown_until_us = 0,
     .valid_flags = {false,false,false,false},
     .all_valid = false,
     .last_ping_time_us = 0,
@@ -363,10 +391,9 @@ void overwrite_receiver_with_ref_cos(int16_t *receiver, const int16_t *ref_cos, 
     }
 }
 
-
-static inline void process_buffer(uint16_t *src, uint64_t end_time_us, const uint64_t startCycle) {
-    swap_buffer_bytes(src,BUFFER_SIZE);
-    deinterleave_and_clear_id(src, BUFFER_SIZE, rec1_arr, rec2_arr,rec3_arr,rec4_arr);
+static inline void prepare_buffers(uint16_t *src) {
+    swap_buffer_bytes(src, BUFFER_SIZE);
+    deinterleave_and_clear_id(src, BUFFER_SIZE, rec1_arr, rec2_arr, rec3_arr, rec4_arr);
     rec1_arr[0] = rec1_arr[1]; // first value is often malformed
     rec2_arr[0] = rec2_arr[1];
     rec3_arr[0] = rec3_arr[1];
@@ -383,24 +410,47 @@ static inline void process_buffer(uint16_t *src, uint64_t end_time_us, const uin
     overwrite_receiver_with_ref_cos(rec4_arr, frequency_states[0].ref_cos, REF_TABLE_SIZE_39kHz, 6);
     */
     //ESP_LOGI(TAG, "Finished overwrite");
-    
+}
 
-    
+
+static inline void process_receivers(uint64_t end_time_us, const uint64_t startCycle) {
+    uint64_t start_time_us = calculate_arrival_time_us(end_time_us, 0, REDUCED_BUFFER_SIZE);
     for (int i = 0; i < NUM_FREQUENCIES; i++) {
         if(frequency_states[i].all_valid) {
-            ESP_LOGI(TAG, "ALL FOUND: %lld", end_time_us);
-            if((end_time_us > frequency_states[i].last_ping_time_us + MAX_SIGNAL_DURATION_US)) {
+            ESP_LOGD(TAG, "ALL FOUND: %lld", end_time_us);
+            if((start_time_us > frequency_states[i].cooldown_until_us)) {
                 reset_frequency_state(&frequency_states[i]);  // Avoid searching for frequency if it has been found. After a duration it can be measured again
                 ESP_LOGI(TAG, "reset frequency max_duration");
             }
             continue;
         }
         // Targeted frequency is reset if not enough receivers noticed it within a realistic timeframe
-        if((frequency_states[i].last_ping_time_us > 0) && (end_time_us > frequency_states[i].last_ping_time_us + MAX_SIGNAL_DIFFERENCE_US)) {
+        if((frequency_states[i].last_ping_time_us > 0) && (start_time_us > frequency_states[i].last_ping_time_us + MAX_SIGNAL_DIFFERENCE_US)) {
             reset_frequency_state(&frequency_states[i]);
-            ESP_LOGI(TAG, "reset frequency max_difference");
+            ESP_LOGD(TAG, "reset frequency max_difference");
             continue;
         }
+        bool any_equal = false;
+        for (int a = 0; a < NUM_RECEIVERS; a++) {
+            for (int b = a + 1; b < NUM_RECEIVERS; b++) {
+                if (frequency_states[i].arrival_times[a] == 0) break;
+                if (frequency_states[i].arrival_times[a] == frequency_states[i].arrival_times[b]) {
+                    any_equal = true;
+                    break;
+                }
+            }
+            if (any_equal) break;
+        }
+
+        if (any_equal) {
+            ESP_LOGW(TAG, "Duplicate arrival times detected, resetting.");
+            reset_frequency_state(&frequency_states[i]);
+            return;
+        }
+
+
+
+        if(error_check_cycle(MAX_CYCLE_COUNT, startCycle, 100)) break;
         ESP_LOGD(TAG, "Before update");
         update_frequency_state(rec1_arr, &frequency_states[i], 0, end_time_us, startCycle);
         if(error_check_cycle(MAX_CYCLE_COUNT, startCycle, 0)) break;
@@ -469,8 +519,9 @@ void processing_task(void *pvParameters) {
                                       ((buffer_ping[1] &0xF000) != 0x1000) ||
                                       ((buffer_ping[2] &0xF000) != 0x2000) ||
                                       ((buffer_ping[3] &0xF000) != 0x3000)) ? misalignedSigCount +1 : 0;
+                prepare_buffers(buffer_ping);
                 startCycleCount = get_ccount();
-                process_buffer(buffer_ping, event.end_time_us, startCycleCount);
+                process_receivers(event.end_time_us,startCycleCount);
                 endCycleCount = get_ccount();
                 ESP_LOGD(TAG, "TIME: %lld, CYCLES: %lld", event.end_time_us, endCycleCount-startCycleCount);
                 ESP_ERROR_CHECK(spi_device_queue_trans(spi, &trans_ping, portMAX_DELAY)); // requeue the ping transaction
@@ -487,8 +538,9 @@ void processing_task(void *pvParameters) {
                 */
                 //print_buffer_csv_inline(rec1_arr, REDUCED_BUFFER_SIZE);
             } else {
+                prepare_buffers(buffer_ping);
                 startCycleCount = get_ccount();
-                process_buffer(buffer_pong, event.end_time_us, startCycleCount);
+                process_receivers(event.end_time_us,startCycleCount);
                 endCycleCount = get_ccount();
                 ESP_ERROR_CHECK(spi_device_queue_trans(spi, &trans_pong, portMAX_DELAY)); 
                 if(printCount % 2000 == 0) {
